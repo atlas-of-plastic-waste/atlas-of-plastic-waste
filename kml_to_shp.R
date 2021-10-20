8d820c2362e25f86f37d0818c28bfd4f90a7a12c
mdgordo <mdgordo@gmail.com>
2021-10-20 10:33:04 -0400

    kml to shapefile workflow

---
+++ kml_to_shp.R
@@ -0,0 +1,10 @@
+library(sf)
+library(tidyverse)
+
+apw.shp <- st_read("atlas_of_plastic.kml")
+
+apw.shp <- separate(apw.shp, col = "Description", into = c("Country", "site_type", "waste_type", "year", "observer"), sep = ", ")
+
+apw.shp <- st_zm(apw.shp, drop = TRUE, what = "ZM")
+
+st_write(apw.shp, "apw/atlas_of_plastic.shp")
